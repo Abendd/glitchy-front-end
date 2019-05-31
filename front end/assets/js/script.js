210605d3d22d9715e522b596e2bc4103a345f03f
Abend <ktkartiktyagi98@gmail.com>
2019-05-31 21:50:24 +0530

    First commit

---
+++ front end/assets/js/script.js
@@ -0,0 +1,19 @@
+onSubmit = (event) =>{
+	let email = document.getElementById("Email").value;
+	let url = document.getElementById("ur").value;
+	fetch('https://tranquil-cliffs-88305.herokuapp.com/register',{
+		method:'POST',
+		headers:{'Content-Type':'application/json'},
+		body:JSON.stringify({
+			email:email,
+			url:url
+		})
+	})
+	.then(response=> response.json())
+	.then(response=>{
+		console.log(response);
+		window.location.href = "file:///D:/front%20end/thanks1.html";
+	})
+	
+
+}
